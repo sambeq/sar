a3b112b2d0e32d21022ea813efcf298faabb3385
sambeq <sambeq11@htl-shkoder.com>
2016-12-18 00:29:38 +0100

    space

--- dbconnect.php
+++ dbconnect.php
@@ -7,6 +7,7 @@
 	try {
 		$dbh = new PDO($url, $usr, $pwd);
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
 		
 		} catch (PDOException $e) {
 		print "Error!: " . $e->getMessage() . "<br/>";
