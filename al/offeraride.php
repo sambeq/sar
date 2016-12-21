b74f19d4541cdabd3abb1f52bd1524ecb10fa192
sambeq <sambeq11@htl-shkoder.com>
2016-12-21 10:34:38 +0100

    Add files via upload

---
+++ al/offeraride.php
@@ -0,0 +1,65 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>OFFER A RIDE</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		
+		<!-- Bootstrap Core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		
+		<!-- Custom CSS -->
+		<link href="css/custom.css" rel="stylesheet">
+		
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
+		
+		<!-- Bootstrap Core JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+		
+		<meta charset="utf-8">
+		
+		<meta name="description" content="The first carpooling company in Albania." />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="generator" content="Codeply">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+		<link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.1/animate.min.css" rel="stylesheet" />
+		<link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+		<link rel="stylesheet" href="css/styles.css" />
+		
+	</head>
+	
+	
+	<body>
+		<?php
+			include 'navbar.php';
+		?>
+		
+		<section class="bg-primary" id="one">
+			<div class="container">    
+				<div class="row">
+					<div class="col-sm-12">
+						<form class="form-horizontal" action="searchCategory.php" method="post">
+							<fieldset>
+								
+								<!-- Form Name -->
+								<legend style= "color:red">OFFER A RIDE</legend>
+								
+								<h2 style="color:yellow" align="center" >Coming Soon</h2>
+								
+							</fieldset>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php
+		include 'footer.php';
+	?>
+	
+</body>
+</html>
+
