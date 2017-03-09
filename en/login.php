0715d37212005b6df759d66890d209a0fe65e956
sambeq <sambeq11@htl-shkoder.com>
2017-03-09 09:11:13 +0100

    Last updated 09/03/2017. 1) Es wurde die login über Facebook und Google ermöglicht. Ich habe viel Zeit verbracht um Recherche zu machen. Es war komplex  weil der Code neu war.

--- en/login.php
+++ en/login.php
@@ -1,9 +1,60 @@
+<?php
+//Include FB config file && User class
+require_once '../facebook/fbConfig.php';
+require_once '../facebook/User.php';
+
+if(!$fbUser){
+    $fbUser = NULL;
+    $loginURL = $facebook->getLoginUrl(array('redirect_uri'=>$redirectURL,'scope'=>$fbPermissions));
+    $output = '<a href="'.$loginURL.'"><img src="../facebook/images/fblogin-btn.png"></a>';
+}else{
+    //Get user profile data from facebook
+    $fbUserProfile = $facebook->api('/me?fields=id,first_name,last_name,email,link,gender,locale,picture');
+
+    //Initialize User class
+    $user = new User();
+
+    //Insert or update user data to the database
+    $fbUserData = array(
+        'oauth_provider'=> 'facebook',
+        'oauth_uid' 	=> $fbUserProfile['id'],
+        'first_name' 	=> $fbUserProfile['first_name'],
+        'last_name' 	=> $fbUserProfile['last_name'],
+        'email' 		=> $fbUserProfile['email'],
+        'gender' 		=> $fbUserProfile['gender'],
+        'locale' 		=> $fbUserProfile['locale'],
+        'picture' 		=> $fbUserProfile['picture']['data']['url'],
+        'link' 			=> $fbUserProfile['link']
+    );
+    $userData = $user->checkUser($fbUserData);
+
+    //Put user data into session
+    $_SESSION['userData'] = $userData;
+
+    //Render facebook profile data
+    if(!empty($userData)){
+        $output = '<h1>Facebook Profile Details </h1>';
+        $output .= '<img src="'.$userData['picture'].'">';
+        $output .= '<br/>Facebook ID : ' . $userData['oauth_uid'];
+        $output .= '<br/>Name : ' . $userData['first_name'].' '.$userData['last_name'];
+        $output .= '<br/>Email : ' . $userData['email'];
+        $output .= '<br/>Gender : ' . $userData['gender'];
+        $output .= '<br/>Locale : ' . $userData['locale'];
+        $output .= '<br/>Logged in with : Facebook';
+        $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Facebook Page</a>';
+        $output .= '<br/>Logout from <a href="logout.php">Facebook</a>';
+    }else{
+        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>LOG IN</title>
 
         <?php include 'head.php'; ?>
+
 		
 	</head>
 	<body>
@@ -11,6 +62,7 @@
     <?php include 'navbar.php'; ?>
 		
 		<section style= "color:black" class="bg-primary" id="one">
+
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-12">
@@ -48,11 +100,21 @@
 													<button name="login" type="submit" class="btn btn-success btn-md">Log In</button>
 													<button type="reset" class="btn btn-danger btn-md">Reset</button>
 												</div>
+
 											</div>
 											<div class="col-sm-offset-3 col-sm-9">
 												<div class="form-group last">Not Registered? <a href="singUp.php" class="">Register here</a>
 												</div>
 											</div>
+                                            <div><?php echo $output; ?></div>
+
+                                            <div class="form-group last">
+                                                <div class="col-sm-offset-3 col-sm-9">
+                                                    <a href="../facebook" type="button" class="btn btn-success btn-md">Facebook</a>
+                                                    <a href="../google" type="button" class="btn btn-danger btn-md">Gmail</a>
+                                                </div>
+
+                                            </div>
 										</form>
 									</div>
                                 </div>
